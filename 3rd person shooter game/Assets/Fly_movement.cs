aa8be23b020abe60e9d91e32bd9ae177ef09690d
xxMiess <michelledekinkelder@gmail.com>
2018-03-14 15:21:25 +0100

    Movement
    basic movement 1.0

---
+++ 3rd person shooter game/Assets/Fly_movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fly_movement : MonoBehaviour {
+
+    public float speed = 10.0F;
+    public float rotationSpeed = 100.0F;
+    void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        translation *= Time.deltaTime;
+        rotation *= Time.deltaTime;
+        transform.Translate(0, 0, translation);
+        transform.Rotate(0, rotation, 0);
+
+        if(Input.GetKey("space"))
+        {
+            transform.Translate(0, y: 0.3f, z: 0);
+        }
+
+        transform.Translate(0, -0.1f, 0);
+    }
+}

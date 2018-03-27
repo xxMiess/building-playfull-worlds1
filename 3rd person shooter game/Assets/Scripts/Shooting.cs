90288c785b5ac3ed7b27608674da5762805ad0e7
xxMiess <michelledekinkelder@gmail.com>
2018-03-27 13:31:22 +0200

    Shooting
    Code for shooting added

---
+++ 3rd person shooter game/Assets/Scripts/Shooting.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour {
+    [SerializeField]
+    private Camera cam;
+    [SerializeField]
+    private LayerMask mask;
+    //public Transform player;
+
+	// Use this for initialization
+	void Start () {
+		if (cam == null)
+        {
+            Debug.LogError("no camera reference");
+            this.enabled = false;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey("f"))
+        {
+            Shoot();
+        }
+	}
+
+    void Shoot ()
+    {
+        //player = GameObject.FindWithTag("Player").transform;
+
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position,cam.transform.forward,out hit, 10f, mask))
+        {
+            Debug.Log(hit.collider.name);
+            Debug.DrawRay(cam.transform.position, cam.transform.forward * 10f, Color.green, 5.0f);
+        }
+    }
+}
